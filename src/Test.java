f96585bd9e8bdb7d27aed4879a63cbfacf81f9ee
Oleksiy <o.v.sirobaba@student.khai.edu>
2022-10-08 21:48:24 +0200

    No conflict anymore :)

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Sample Text");
+        System.out.println("Hello, teacher! Some education here I have ;)");
     }
 }
