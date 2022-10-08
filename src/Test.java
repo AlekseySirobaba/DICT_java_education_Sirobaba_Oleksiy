a1d6f7cccf63d34151bbd14f7901d9cb82ae0d3a
Oleksiy <o.v.sirobaba@student.khai.edu>
2022-10-08 21:49:11 +0200

    No conflict anymore :)

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, teacher! Some education here I have ;)");
+        System.out.println("Interesting Java education");
+        // Hello, teacher :) Have a nice day!
+        System.out.println("DICT");
     }
 }
