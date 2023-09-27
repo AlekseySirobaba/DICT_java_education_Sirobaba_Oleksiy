80590b08a1004120a0288994677b784cc79c3562
AlekseySirobaba <o.v.sirobaba@student.khai.edu>
2023-09-27 11:29:56 +0200

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Somebody once told me... The world is gonna roll me");
+        System.out.println("Somebody once told me... The Art of War");
+        // ‘Now I Am Become Death, the Destroyer of Worlds.’ - Oppenheimer
+        System.out.println("Somebody once told me...");
+        System.out.println("The world is gonna roll me");
     }
 }
