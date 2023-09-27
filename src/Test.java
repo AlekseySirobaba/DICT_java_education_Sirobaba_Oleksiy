4d2969e3448ad63bb9af483b7096327918f0d542
AlekseySirobaba <o.v.sirobaba@student.khai.edu>
2023-09-27 11:28:11 +0200

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("The Art of War");
+        System.out.println("Somebody once told me... The world is gonna roll me");
     }
 }
